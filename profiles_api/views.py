f077fd5aee25576dff6b6c2990e0c53ca5d90b5e
Jyoti Kj <speak2pearl@gmail.com>
2020-06-30 17:38:41 +0530

    Viewset added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,53 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import serializers
 from rest_framework import status
+
+
+class HelloViewSets(viewsets.ViewSet):
+    """Test APi View sets"""
+
+    serializer_class = serializers.HelloSerializers
+
+    def list(self, request):
+
+        an_apiviewsets = [
+            'Uses HTTP method as functions - list, create, retrieve, update,partial update, Destroy',
+            'It is similar to traditional django viewsets',
+            'Gives you the most control over you logic',
+            'It mapped manually to the URLs'
+        ]
+
+        return Response({'message':'Hello',  'an_apiviewsets': an_apiviewsets})
+
+    def create(self, request):
+        serializer =  self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message' : message})
+        else:
+            return Response(
+                    serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handles getting in an object by its Id"""
+        return Response({'HTTP method': 'Get'})
+
+    def update(self, request, pk=None):
+        """ Handles updating an object"""
+        return Response({'HTTP Method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """ Handles updating an part of an object"""
+        return Response({'HTTP Method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """ Handles removing an object """
+        return Response({'HTTP Method' : 'DELETE'})
 
 
 class HelloApiView(APIView):
