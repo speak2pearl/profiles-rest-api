5a3c4b638daaca9375e028d40c5e101b9e93a382
Jyoti Kj <speak2pearl@gmail.com>
2020-07-01 15:38:16 +0530

    Added Search

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -4,7 +4,7 @@ class UpdateOwnProfile(permissions.BasePermission):
     """ Check user is trying to edit their own profile """
 
     def has_object_permission(self,request,view,obj):
-        """ Check user is trying to edit their own permission """
+        """ Check user is trying to edit their own profile """
 
         if request.method in permissions.SAFE_METHODS:
             return True
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,7 +6,7 @@ from . import serializers
 from rest_framework import status
 
 from profiles_api import models
-
+from rest_framework import filters
 from profiles_api import permissions
 from rest_framework.authentication import TokenAuthentication
 
@@ -105,7 +105,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
-
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email', 'address',)
 
 
     # authentication_classes = (TokenAuthentication,)
