d4b12bd9468f9287f185e4304e91617e64926bae
Jyoti Kj <speak2pearl@gmail.com>
2020-07-01 16:24:30 +0530

    Addeed Login token autherization

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -9,6 +9,9 @@ from profiles_api import models
 from rest_framework import filters
 from profiles_api import permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 
 class HelloViewSets(viewsets.ViewSet):
     """Test APi View sets"""
@@ -107,6 +110,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email', 'address',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """ Check Email name and password and returns Auth token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
     # authentication_classes = (TokenAuthentication,)
